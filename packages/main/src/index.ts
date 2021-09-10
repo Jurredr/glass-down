460bf72c1ff67376e7f7c0e76b6062b39b8697be
Jurredr <github@jurre.me>
2021-09-10 23:19:22 +0200

    Created a custom menu with file management base

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,9 +1,11 @@
-import { app, BrowserWindow, nativeImage } from 'electron'
-import { join } from 'path'
-import { platform } from 'process'
+import type { MenuItemConstructorOptions } from 'electron';
+import { dialog } from 'electron'
+import { app, BrowserWindow, Menu, nativeImage, shell } from 'electron'
+import * as path from 'path'
 import { URL } from 'url'
 
 const isSingleInstance = app.requestSingleInstanceLock()
+const isMac = process.platform === 'darwin'
 
 if (!isSingleInstance) {
   app.quit()
@@ -16,10 +18,9 @@ app.disableHardwareAcceleration()
 let mainWindow: BrowserWindow | null = null
 
 const createWindow = async () => {
-  const iconPath =
-    platform === 'darwin'
-      ? join(__dirname, '../../../buildResources/icon.icns')
-      : join(__dirname, '../../../buildResources/icon.png')
+  const iconPath = isMac
+    ? path.join(__dirname, '../../../buildResources/icon.icns')
+    : path.join(__dirname, '../../../buildResources/icon.png')
 
   mainWindow = new BrowserWindow({
     show: false, // Use 'ready-to-show' event to show window
@@ -29,15 +30,138 @@ const createWindow = async () => {
     icon: iconPath,
     webPreferences: {
       nativeWindowOpen: true,
-      preload: join(__dirname, '../../preload/dist/index.cjs'),
+      preload: path.join(__dirname, '../../preload/dist/index.cjs'),
       contextIsolation: import.meta.env.MODE !== 'test', // Spectron tests can't work with contextIsolation: true
       enableRemoteModule: import.meta.env.MODE === 'test' // Spectron tests can't work with enableRemoteModule: false
     }
   })
 
+  // Set menu
+  const menuTemplate: Electron.MenuItemConstructorOptions[] = [
+    ...((isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: 'about' },
+              { type: 'separator' },
+              { role: 'services' },
+              { type: 'separator' },
+              { role: 'hide' },
+              { role: 'hideOthers' },
+              { role: 'unhide' },
+              { type: 'separator' },
+              { role: 'quit' }
+            ]
+          }
+        ]
+      : []) as MenuItemConstructorOptions[]),
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'New File',
+          accelerator: "CmdOrCtrl+N",
+          click: () => console.log('test')
+        },
+        {
+          label: 'New Window',
+          accelerator: "CmdOrCtrl+Shift+N",
+          click: () => console.log('test')
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Open Recent',
+          role: 'recentdocuments',
+          submenu: [
+            {
+              label: 'Clear Recent',
+              role: 'clearrecentdocuments'
+            }
+          ]
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Save',
+          accelerator: "CmdOrCtrl+S",
+          click: () => console.log('save')
+        },
+        {
+          label: 'Save As',
+          accelerator: "CmdOrCtrl+Shift+S",
+          click: () => dialog.showSaveDialog(
+            {
+              defaultPath: path.resolve(app.getPath("desktop"), path.basename('http://www.example.com/path/to/file.jpg'))
+            }
+          )
+        }
+      ] as MenuItemConstructorOptions[]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...((isMac
+          ? [
+              { role: 'pasteAndMatchStyle' },
+              { role: 'delete' },
+              { role: 'selectAll' },
+              { type: 'separator' },
+              {
+                label: 'Speech',
+                submenu: [{ role: 'startSpeaking' }, { role: 'stopSpeaking' }]
+              }
+            ]
+          : [
+              { role: 'delete' },
+              { type: 'separator' },
+              { role: 'selectAll' }
+            ]) as MenuItemConstructorOptions[])
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...((isMac
+          ? [
+              { type: 'separator' },
+              { role: 'front' },
+              { type: 'separator' },
+              { role: 'window' }
+            ]
+          : [{ role: 'close' }]) as MenuItemConstructorOptions[])
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: "Glassdown's GitHub page",
+          click: async () => {
+            await shell.openExternal('https://github.com/Jurredr/glass-down')
+          }
+        }
+      ]
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(menu)
+
   // Set mac dock image
   const image = nativeImage.createFromPath(
-    join(__dirname, '../../../buildResources/icon.png')
+    path.join(__dirname, '../../../buildResources/icon.png')
   )
   app.dock.setIcon(image)
 
